fc0005dbc0e51791a725431c1edd4ccb953c2b98
Gokay Toga <97601328+GokayToga@users.noreply.github.com>
2025-11-20 12:37:49 +0100

    Update setup_env.bat

--- setup_env.bat
+++ setup_env.bat
@@ -11,7 +11,7 @@ REM Upgrade pip
 pip install --upgrade pip
 
 REM Install MuJoCo and viewer
-pip install mujoco mujoco-python-viewer numpy opencv-python
+pip install mujoco==3.2.0 mujoco-python-viewer numpy opencv-python
 
 echo === Environment ready! ===
 echo Run: ".venv\Scripts\activate" to activate the environment next time.
